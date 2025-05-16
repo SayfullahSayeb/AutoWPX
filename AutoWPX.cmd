b90e764b33fcea69db9e6ab57fe047f41d75bb07
Sayfullah Sayeb <sayfullah.sayeb@gmail.com>
2025-05-16 08:13:31 +0600

    Update AutoWPX.cmd

--- AutoWPX.cmd
+++ AutoWPX.cmd
@@ -11,26 +11,26 @@ echo " / ___ \ |_| | || (_) \ V  V / |  __//  \               "
 echo "/_/   \_\__,_|\__\___/ \_/\_/  |_|  /_/\_\              "
 echo.                                                         "
 echo   Author    : Sayfullah Sayeb                            "
-echo   Version   : 1.0.2                                      "
+echo   Version   : 1.0.3                                      "
 echo   Source    : https://github.com/SayfullahSayeb/AutoWPX  "
 echo   Details   : WordPress Auto Setup Script for XAMPP      "
 echo ========================================================="
 echo.
 
-:: XAMPP paths
-set XAMPP=C:\xampp
-set MYSQL_EXE=%XAMPP%\mysql\bin\mysql.exe
+:: === XAMPP paths ===
+set "XAMPP=C:\xampp"
+set "MYSQL_EXE=%XAMPP%\mysql\bin\mysql.exe"
 
-:: Ask for domain
+:: === Ask for domain ===
 echo ===============================
 set /p DOMAIN=Enter your domain (e.g., mysite.local): 
-set SITE_DIR=%XAMPP%\htdocs\%DOMAIN%
-set DB_NAME=%DOMAIN%
+set "SITE_DIR=%XAMPP%\htdocs\%DOMAIN%"
+set "DB_NAME=%DOMAIN%"
 
-:: Create site directory
+:: === Create site directory ===
 if not exist "%SITE_DIR%" mkdir "%SITE_DIR%"
 
-:: Download and extract WordPress directly into target dir
+:: === Download and extract WordPress ===
 echo Downloading WordPress...
 cd /d "%SITE_DIR%"
 powershell -Command "Invoke-WebRequest -Uri https://wordpress.org/latest.zip -OutFile 'latest.zip'"
@@ -41,17 +41,21 @@ xcopy /E /I /Y ".\wordpress\*" "." >nul
 rmdir /S /Q ".\wordpress"
 del latest.zip
 
-:: Create MySQL database
+:: === Create MySQL database ===
 echo Creating database...
 "%MYSQL_EXE%" -u root -e "CREATE DATABASE IF NOT EXISTS `%DB_NAME%`;"
 
-:: Add to hosts file
+:: === Setup wp-config.php ===
+copy "wp-config-sample.php" "wp-config.php" >nul
+powershell -Command "(Get-Content 'wp-config.php') -replace 'database_name_here', '%DB_NAME%' -replace 'username_here', 'root' -replace 'password_here', '' | Set-Content 'wp-config.php'"
+
+:: === Add to hosts file ===
 findstr /C:"%DOMAIN%" C:\Windows\System32\drivers\etc\hosts >nul || (
     echo 127.0.0.1 %DOMAIN% >> C:\Windows\System32\drivers\etc\hosts
 )
 
-:: Set up Virtual Host
-set VHOST_CONF=%XAMPP%\apache\conf\extra\httpd-vhosts.conf
+:: === Set up Virtual Host ===
+set "VHOST_CONF=%XAMPP%\apache\conf\extra\httpd-vhosts.conf"
 findstr /C:"ServerName %DOMAIN%" "%VHOST_CONF%" >nul || (
     echo Adding virtual host...
     (
@@ -68,12 +72,12 @@ findstr /C:"ServerName %DOMAIN%" "%VHOST_CONF%" >nul || (
     ) >> "%VHOST_CONF%"
 )
 
-:: Ensure Apache includes vhost file
+:: === Ensure Apache includes vhost file ===
 findstr /C:"Include conf/extra/httpd-vhosts.conf" "%XAMPP%\apache\conf\httpd.conf" >nul || (
     echo Include conf/extra/httpd-vhosts.conf >> "%XAMPP%\apache\conf\httpd.conf"
 )
 
-:: Launch install page
+:: === Launch site install page ===
 echo Done! Opening site...
 start http://%DOMAIN%/wp-admin/install.php
 pause
